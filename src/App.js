2e0720f0a83966ca19de5d14a8f98f6f4cb7f986
Alexander-Welsh <alex.n.welsh@gmail.com>
2018-04-07 10:50:43 -0700

    Added all basic features

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Random "image not found" image from google search, no license necessary
+const imgNotFound = "https://upload.wikimedia.org/wikipedia/commons/2/26/512pxIcon-sunset_photo_not_found.png"
+
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
       menuItems: [],
-      value: ""
+      value: "",
+      addToMeal: ""
     };
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    console.log(this.state.menuItems)
   }
 
   handleChange(event){
@@ -28,31 +34,37 @@ class App extends Component {
       console.log(data)
       let menuItems = data.results.map((item, iter) =>{
         return(
-            <div key={iter}>
-              <img src={item.thumbnail} alt={item.title}/>
-              <a href={item.href}>Learn More!</a>
+            <div className="items" key={iter}>
+              <h3>{item.title}</h3>
+              <a href={item.href}>
+                <img className="thumbnail" src={item.thumbnail ? item.thumbnail : imgNotFound} title={item.title} alt="Click here to see more"/>
+              </a>
+              <p><strong>Ingredients:</strong> {item.ingredients}</p>
 
             </div>
           )
       })
-      this.setState({menuItems: menuItems});
+      this.setState({
+        menuItems: menuItems,
+        addToMeal: "Click on a dish to add it to a meal!"
+      });
     })
     event.preventDefault();
   }
 
 
   render() {
-    console.log(this.state)
     return (
       <div className="App">
+      <h1> Search for a recipe!</h1>
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
-        <div>
+        <h2 id="add-meal-text">{this.state.addToMeal}</h2>
+        <div className="item-holder">
             {this.state.menuItems}
         </div>
       </div>
