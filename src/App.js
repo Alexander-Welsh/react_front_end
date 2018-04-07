61067822b722c7011bceaf0f5e0654922bfd0fbd
Alexander-Welsh <alex.n.welsh@gmail.com>
2018-04-07 14:33:44 -0700

    final features added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // Random "image not found" image from google search, no license necessary
@@ -12,35 +11,51 @@ class App extends Component {
     this.state = {
       menuItems: [],
       value: "",
-      addToMeal: ""
+      addToMeal: "",
+      meal: [],
+      mealExists:""
     };
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    console.log(this.state.menuItems)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   handleChange(event){
     this.setState({value: event.target.value});
   }
 
+  handleAdd(event, id, recipe){
+    let mealArray = this.state.meal.concat(recipe.title)
+    this.setState({
+      meal: mealArray,
+      mealExists: "Your meal:"
+
+    })
+  }
+
+  handleDelete(event, id, item){
+    var array = this.state.meal
+    var index = array.indexOf(item)
+    array.splice(index, 1)
+    this.setState({meal: array})
+  }
+
   handleSubmit(event){
-    console.log("event")
+    event.preventDefault();
     fetch(`http://www.recipepuppy.com/api/?q=${this.state.value}`)
     .then(results => {
-      console.log(results)
       return results.json();
     }).then(data => {
-      console.log(data)
       let menuItems = data.results.map((item, iter) =>{
         return(
-            <div className="items" key={iter}>
+            <div className="items" key={iter} onClick={(e) => this.handleAdd(e, iter, item)}>
               <h3>{item.title}</h3>
               <a href={item.href}>
                 <img className="thumbnail" src={item.thumbnail ? item.thumbnail : imgNotFound} title={item.title} alt="Click here to see more"/>
               </a>
               <p><strong>Ingredients:</strong> {item.ingredients}</p>
-
             </div>
           )
       })
@@ -49,23 +64,35 @@ class App extends Component {
         addToMeal: "Click on a dish to add it to a meal!"
       });
     })
-    event.preventDefault();
+
   }
 
 
   render() {
     return (
       <div className="App">
-      <h1> Search for a recipe!</h1>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <h1> Search for a recipe!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <h2 id="add-meal-text">{this.state.addToMeal}</h2>
-        <div className="item-holder">
+        <h2 id="show-if-meal">{this.state.mealExists}</h2>
+        <div id="box-container">
+          <div className="item-holder">
             {this.state.menuItems}
+          </div>
+          <div id="meal-items">
+            <ol>
+            {this.state.meal.map((item, iter)=>{
+              return[
+                  <li key={iter}>{item}</li>, <button key={iter + 50} className="small-remove" onClick={(e) => this.handleDelete(e, iter, item)}> Remove </button>
+                ]
+            })}
+            </ol>
+          </div>
         </div>
       </div>
     );
